005db4e593b5c95f2484e7eeedb49c3932f90496
Nikolay Stepanov <stepanovnikola96@mail.ru>
2019-12-18 23:26:28 +0300

    created a project

---
+++ src/test/java/ru/rosbank/javaschool/slack/SlackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rosbank.javaschool.slack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
